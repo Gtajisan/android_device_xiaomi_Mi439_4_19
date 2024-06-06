b466c3bc9c7d88ad97ed7af062e1b7af82c413ee
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-06-06 16:25:26 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Mi439_4_19-user
-add_lunch_combo omni_Mi439_4_19-userdebug
-add_lunch_combo omni_Mi439_4_19-eng
+add_lunch_combo lineage_Mi439_4_19-user
+add_lunch_combo lineage_Mi439_4_19-userdebug
+add_lunch_combo lineage_Mi439_4_19-eng
